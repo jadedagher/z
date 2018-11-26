0ec1657f6ef05f6fd09df15aac1222d2acdf7289
jadedagher <jadedagher@aol.com>
2018-11-26 22:12:39 +0100

    new design cards events

--- api/server/product.js
+++ api/server/product.js
@@ -6,7 +6,8 @@ Meteor.publish("products", function() {
   fields: { 
   	brand_ID: 1,
   	brand_name: 1,
-  	product_name: 1, 
+  	product_name: 1,
+    product_img: 1, 
   	product_category: 1, 
   	product_initialPrice: 1,
     product_description: 1,
